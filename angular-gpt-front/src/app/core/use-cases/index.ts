fac64fc87c27ec479ae5fb37a1fda6ca13ee80d7
Victor Manuel <vmherasdurillo@hotmail.com>
2024-08-11 17:22:12 +0200

    Archivos de barril y traducciones
    Se han echo pequeños cambios a los archivos de barril para simplificar y se han añadido las traducciones a los idiomas que llegaban desde la api de iso-639-1
    
    Añadido a esto se ha desinstalado i18n-iso-countries

--- angular-gpt-front/src/app/core/use-cases/index.ts
+++ angular-gpt-front/src/app/core/use-cases/index.ts
@@ -2,3 +2,6 @@ export * from './orthography/orthography.use-case';
 export * from './pros-cons/pros-cons.use-case';
 
 export * from './pros-cons/pros-cons-stream.use-case';
+
+export * from './translate/translate.use-case';
+export * from './translate/translate-stream.use-case';
--- angular-gpt-front/src/app/interfaces/index.ts
+++ angular-gpt-front/src/app/interfaces/index.ts
@@ -3,3 +3,5 @@ export * from './message.interfaces';
 export * from './orthography.response';
 
 export * from './pros-cons.response';
+
+export * from './translate.response';
