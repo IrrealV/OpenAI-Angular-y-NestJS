a11c4984ca3c8691dc5847462721483a3d3790e2
Victor Manuel <vmherasdurillo@hotmail.com>
2024-08-25 13:19:03 +0200

    Devolver URL del servidor

--- nest-gpt-back/src/gpt/use-cases/image-generation.use-case.ts
+++ nest-gpt-back/src/gpt/use-cases/image-generation.use-case.ts
@@ -1,5 +1,6 @@
 import OpenAI from 'openai';
 import { downloadImageAsPng } from 'src/helpers';
+import * as fs from 'fs';
 
 interface Options {
   prompt: string;
@@ -13,18 +14,44 @@ export const imageGenerationUseCase = async (
   const { prompt, originalImage, maskImage } = options;
 
   //TODO: verificar imagen original
-  const response = await openai.images.generate({
-    prompt: prompt,
+  if (!originalImage || !maskImage) {
+    const response = await openai.images.generate({
+      prompt: prompt,
+      model: 'dall-e-3',
+      n: 1,
+      size: '1024x1024',
+      quality: 'standard',
+      response_format: 'url',
+    });
+
+    //TODO: Guardar imagen en FS.
+
+    const fileName = downloadImageAsPng(response.data[0].url);
+    const url = `${process.env.SERVER_URL}/gpt/gpt/image-generation/${fileName}`;
+
+    return {
+      url: url,
+      openAiUrl: response.data[0].url,
+      revised_prompt: response.data[0].revised_prompt,
+    };
+  }
+  //originalImage= http://localhost:3000/gpt/image-generation/1724579399463.png
+  //maskImage = Base64; asdfasdfadfSDSDFASDFSDFASDFsdfasdfsdf
+  const pgnImagePath = await downloadImageAsPng(originalImage);
+  const maskPath = await downloadImageAsPng(maskImage);
+
+  const response = await openai.images.edit({
     model: 'dall-e-3',
+    prompt: prompt,
+    image: fs.createReadStream(pgnImagePath),
+    mask: fs.createReadStream(maskPath),
     n: 1,
     size: '1024x1024',
-    quality: 'standard',
     response_format: 'url',
   });
 
-  //TODO: Guardar imagen en FS.
-  const url = downloadImageAsPng(response.data[0].url);
-
+  const fileName = downloadImageAsPng(response.data[0].url);
+  const url = `${process.env.SERVER_URL}/gpt/gpt/image-generation/${fileName}`;
   return {
     url: url,
     openAiUrl: response.data[0].url,
--- nest-gpt-back/src/helpers/download-image-as-png.ts
+++ nest-gpt-back/src/helpers/download-image-as-png.ts
@@ -18,9 +18,34 @@ export const downloadImageAsPng = async (url: string) => {
   const buffer = Buffer.from(await response.arrayBuffer());
 
   /* fs.writeFileSync(`${folderPath}/${imageNamePng}`, buffer); */
-  const completePath = path.join(folderPath, imageNamePng);
+  /* 
+  return completePath(buffer, folderPath, imageNamePng); */
+  return imageNamePng;
+};
 
+export const downloadBase64ImageAsPng = async (base64Image: string) => {
+  // Remover encabezado
+  base64Image = base64Image.split(';base64,').pop();
+  const imageBuffer = Buffer.from(base64Image, 'base64');
+
+  const folderPath = path.resolve('./', './generated/images/');
+  fs.mkdirSync(folderPath, { recursive: true });
+
+  const imageNamePng = `${new Date().getTime()}-64.png`;
+
+  // Transformar a RGBA, png // Así lo espera OpenAI
+  /* 
+  return completePath(imageBuffer, folderPath, imageNamePng); */
+  return imageNamePng;
+};
+
+async function completePath(
+  buffer: Buffer,
+  folderPath: string,
+  imageNamePng: string
+) {
+  const completePath = path.join(folderPath, imageNamePng);
   await sharp(buffer).png().ensureAlpha().toFile(completePath);
 
   return completePath;
-};
+}
