fe088059c61f8a22169f15d030b6218511bd503b
Victor Manuel <vmherasdurillo@hotmail.com>
2024-09-02 16:11:47 +0200

    Update message.interfaces.ts
    Changed Interfaces

--- angular-gpt-front/src/app/interfaces/message.interfaces.ts
+++ angular-gpt-front/src/app/interfaces/message.interfaces.ts
@@ -1,10 +1,18 @@
 export interface Message {
   text: string;
   isGpt: boolean;
-  info?: {
-    userScore: number;
-    errors: string[];
-    message: string;
-  };
+  info?: Info;
   audioUrl?: string;
+  imageInfo?: ImageInfo;
+}
+
+interface Info {
+  userScore: number;
+  errors: string[];
+  message: string;
+}
+
+interface ImageInfo {
+  url: string;
+  alt: string;
 }
