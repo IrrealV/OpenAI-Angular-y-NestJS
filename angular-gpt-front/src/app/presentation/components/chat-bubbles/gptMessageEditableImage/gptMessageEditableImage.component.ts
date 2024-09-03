8382a053bb8315a14fc054637c3e7d889fe26d24
Victor Manuel <vmherasdurillo@hotmail.com>
2024-09-03 19:45:58 +0200

    Dibujar y recortar imagen Canvas
    Se han añadido las funciones de canvas para recortar y editar las imagenes generadas

--- angular-gpt-front/src/app/presentation/components/chat-bubbles/gptMessageEditableImage/gptMessageEditableImage.component.ts
+++ angular-gpt-front/src/app/presentation/components/chat-bubbles/gptMessageEditableImage/gptMessageEditableImage.component.ts
@@ -1,5 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  signal,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-gpt-message-editable-image',
@@ -8,13 +17,86 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './gptMessageEditableImage.component.html',
   styleUrl: './gptMessageEditableImage.component.css',
 })
-export class GptMessageEditableImageComponent {
+export class GptMessageEditableImageComponent implements AfterViewInit {
   @Input({ required: true }) text!: string;
   @Input({ required: true }) imageInfo!: { url: string; alt: string };
 
+  @ViewChild('canvas') canvasElement?: ElementRef<HTMLCanvasElement>;
+
   @Output() onSelectedImage = new EventEmitter<string>();
 
-  handleClick() {
+  public originalImage = signal<HTMLImageElement | null>(null);
+  public isDrawing = signal(false);
+  public coords = signal({ x: 0, y: 0 });
+
+  private canvas!: HTMLCanvasElement;
+  private canvasBounding!: DOMRect;
+
+  ngAfterViewInit(): void {
+    if (!this.canvasElement?.nativeElement) return;
+
+    this.canvas = this.canvasElement.nativeElement;
+    this.canvasBounding = this.canvas.getBoundingClientRect();
+
+    const ctx = this.canvas.getContext('2d');
+
+    const image = new Image();
+    image.crossOrigin = 'Anonymous';
+    image.src = this.imageInfo.url;
+
+    this.originalImage.set(image);
+
+    image.onload = () => {
+      ctx?.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
+    };
+  }
+
+  public onMouseDown(event: MouseEvent) {
+    if (!this.canvas) return;
+    this.isDrawing.set(true);
+
+    //Obtener las coordenadas del mouse relativo al canvas
+    const startX = event.clientX - this.canvasBounding.left;
+    const startY = event.clientY - this.canvasBounding.top;
+
+    // Estos valores son mis coordenadas
+    //console.log({ startX, startY });
+
+    this.coords.set({ x: startX, y: startY });
+  }
+
+  public onMouseMove(event: MouseEvent) {
+    if (!this.isDrawing()) return;
+    if (!this.canvasElement?.nativeElement) return;
+
+    const currentX = event.clientX - this.canvasBounding.left;
+    const currentY = event.clientY - this.canvasBounding.top;
+
+    //Calcular alto y ancho de un rectangulo
+    const width = currentX - this.coords().x;
+    const height = currentY - this.coords().y;
+
+    const canvasWidht = this.canvas.width;
+    const canvasHeight = this.canvas.height;
+
+    //Todo: Limpiar el canvas
+
+    const ctx = this.canvas.getContext('2d')!;
+    ctx.clearRect(0, 0, canvasWidht, canvasHeight);
+    ctx.drawImage(this.originalImage()!, 0, 0, canvasWidht, canvasHeight);
+
+    ctx.clearRect(this.coords().x, this.coords().y, width, height);
+  }
+
+  public onMouseUp() {
+    this.isDrawing.set(false);
+
+    const url = this.canvas.toDataURL('image/png');
+
+    this.onSelectedImage.emit(url);
+  }
+
+  public handleClick() {
     this.onSelectedImage.emit(this.imageInfo.url);
   }
 }
