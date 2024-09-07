64595250eef99208b05286982ff0f2009c9cb547
Victor Manuel <vmherasdurillo@hotmail.com>
2024-09-07 18:11:54 +0200

    Asistente creado
    Se ha creado el chat del asistente

--- angular-gpt-front/src/app/presentation/pages/assistantPage/assistantPage.component.ts
+++ angular-gpt-front/src/app/presentation/pages/assistantPage/assistantPage.component.ts
@@ -1,12 +1,49 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  ChatMessageComponent,
+  MyMessageComponent,
+  TypingLoaderComponent,
+  TextMessageBoxComponent,
+} from '@components/index';
+import { Message } from '@interfaces/message.interfaces';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-assistant-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+  ],
   templateUrl: './assistantPage.component.html',
   styleUrl: './assistantPage.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AssistantPageComponent {}
+export default class AssistantPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiService);
+
+  handleMessage(prompt: string) {
+    console.log({ prompt });
+  }
+
+  /*  handleMessageWithFile({ prompt, file }: TextMessageEventFile) {
+    console.log({ prompt, file });
+  }
+
+  handleMessageWithSelect(event: TextMessageBoxEvent) {
+    console.log(event);
+  } */
+}
